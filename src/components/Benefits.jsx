26b123f564761cc80695b79d2b01a630fe433546
Revanth kumar S <98330771+revanth-004@users.noreply.github.com>
2024-12-09 01:28:30 +0530

    Update Benefits.jsx

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -20,7 +20,7 @@ const Benefits = () => {
               key={item.id}
               className="block relative p-0.5 bg-no-repeat bg-[length:100%_100%] md:max-w-[20rem]"
               style={{
-                backgroundImage: `url(${item.backgroundUrl})`,
+                backgroundImage: `url("${item.backgroundUrl}")`,
               }}
             >
               <div className="relative z-2 flex flex-col min-h-[22rem] p-[2.4rem] pointer-events-none">
